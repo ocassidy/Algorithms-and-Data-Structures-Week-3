01891b2fe9447a3556cf867d58dce6c11f4e0026
oisin cassidy <oisin.c@hotmail.co.uk>
2018-02-15 14:41:28 +0000

    Finished Q6

--- practical3/Sort.h
+++ practical3/Sort.h
@@ -17,37 +17,57 @@
 template <class T>
 void selectionSort(Array<T> & elements) {
 	int min;
+	int comparisons = 0;
+	int movements = 0;
 
 	for (int marker = 0; marker < elements.length() - 1; marker++) {
 		min = marker;
 		for (int i = marker + 1; i < elements.length(); i++) {
-			if (elements[i] < elements[min]) {
+	    	comparisons++;
+	 		if (elements[i] < elements[min]) {
 				min = i;
-			}
+			}		
 		}
 
 		// swap elements at position min and out
-		T tmp = elements[marker];
+		/*T tmp = elements[marker];
 		elements[marker] = elements[min];
-		elements[min] = tmp;
-	}
+		elements[min] = tmp;*/
 
+		std::swap(elements[min], elements[marker]);		
+		movements += 3;
+	}
+	std::cout << "Selection Moves = " << movements << " Comparisons = " << comparisons << std::endl;
 }
 
 // PostCondition: collection elements are sorted
 template <class T>
 void insertionSort(Array<T> & elements) {
 	int in, out, temp;
+	int comparisons = 0;
+	int movements = 0;
+
 	for (out = 1; out < elements.length(); out++) {
-		temp = elements[out];
+		temp = elements[out]; movements++;
+
 		// find position for temp
+		comparisons++;
 		for (in = out; in > 0 && elements[in - 1] >= temp; in--) {
-			elements[in] = elements[in - 1];
+			elements[in] = elements[in - 1]; movements++;
+			comparisons++;
 		}
 		// place temp in new position
-		elements[in] = temp;
+		elements[in] = temp; movements++;
 	}
+	std::cout << "Insertion Moves = " << movements << " Comparisons = " << comparisons << std::endl;
 }
 
+template <class T>
+void swap(T & a, T & b) {  
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
 #endif
 
--- practical3/practical3.cpp
+++ practical3/practical3.cpp
@@ -6,34 +6,37 @@
 //==============================================================================
 
 #include <string>
-#include <cstdlib>	// srand
-#include <ctime>	// time
 #include "Array.h"
 #include "Sort.h"
+#include <random>
 
 using namespace std;
 
 // PostCondition: array is filled with random numbers 1..range
-void fillRandom(Array<int> & data, int range=100) {
-	for (int i = 0; i < data.length(); i++) {
-		data[i] = rand() % 100 + 1;
-	}
-}
-
-// test selection sort
-void testSelect() {
-
-}
-
-// test insertion sort
-void testInsert() {
-
-}
-
+//void fillRandom(Array<int> & data, int range=100) {
+//	for (int i = 0; i < data.length(); i++) {
+//		data[i] = rand() % 100 + 1;
+//	}
+//}
 
 // return difference in the largest and smallest elements in the array  
 int maxDiff(const Array<int> & data) {
+	int min = data[0];
+	int max = data[0];
 
+	for (int i = 0; i < data.length(); i++) {
+		if (data[i] < min) {
+			min = data[i];
+		}
+		if (data[i] > max) {
+			max = data[i];
+		}
+	}
+
+	cout << "Max is: " << max << endl;
+	cout << "Min is: " << min << endl;
+	cout << "Max - min = " << max - min << endl;
+	return (max - min);
 }
 // Compare performance of selection/insertion sort.
 void testBoth() {
@@ -45,17 +48,52 @@ void testString() {
 
 }
 
+int Random() {
+	std::random_device rd; //generator 1
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dis(1, 100);
+	int chance = dis(gen);
+	return chance;
+}
+
+void fillRandom(Array<int> & data) {
+	for (int i = 0; i < data.length(); i++) {
+		data[i] = Random();
+	}
+}
+
+
+// test selection sort
+void testSelect() {
+	Array<int> test(10);
+	fillRandom(test);
+	cout << "Unsorted data: " << test << endl;
+	selectionSort(test);
+	cout << "Sorted data: " << test << endl;
+	cout << endl;	
+}
+
+// test insertion sort
+void testInsert() {
+	Array<int> test(10);
+	//t[0] = 10; t[1] = 9; t[2]=8; t[3] = 7; t[4] = 6; t[5] = 5; t[6] = 4; t[7] = 3; t[8] = 2; t[9] = 1;
+	fillRandom(test);
+	cout << "Unsorted data: " << test << endl;
+	insertionSort(test);
+	cout << "Sorted data: " << test << endl;
+	cout << endl;
+}
+
 // Main method.
 int main() {
 	
-	//initialise the random number generator (used in fillRandom())
-	srand((unsigned)time(NULL));
+	Array<int> t(10);
+	t[0] = 10; t[1] = 9; t[2]=8; t[3] = 7; t[4] = 6; t[5] = 5; t[6] = 4; t[7] = 3; t[8] = 2; t[9] = 1;
 
 	// call test methods here
+	testSelect();
+	testInsert();
+	maxDiff(t);
 
-
-	// ---------------------------------------------------
-	cout << endl << "Press enter to quit";
-	cin.get();
 	return 0;
 }
